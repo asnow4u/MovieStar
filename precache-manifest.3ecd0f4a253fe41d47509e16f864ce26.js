a652887b6a5aac7da1749c79a5c1b5b5a74f672c
Andrew <asnow4u@gmail.com>
2021-03-16 15:54:23 -0600

    Updates

---
+++ precache-manifest.3ecd0f4a253fe41d47509e16f864ce26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc28d7a3d812c8e19e99d198071aeb3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6c8313d87f0a4b3b42a",
+    "url": "/static/css/main.06e0f23e.chunk.css"
+  },
+  {
+    "revision": "c77dd2c2bc8c3dbbddc9",
+    "url": "/static/js/2.8bf49f7a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.8bf49f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c8313d87f0a4b3b42a",
+    "url": "/static/js/main.39fdc4bc.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  }
+]);
