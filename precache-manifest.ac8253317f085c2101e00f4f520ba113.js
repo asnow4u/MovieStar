2d4e67425c733ea4b92b4ca282898928de25dfa9
Andrew <asnow4u@gmail.com>
2021-03-17 17:06:30 -0600

    Updates

---
+++ precache-manifest.ac8253317f085c2101e00f4f520ba113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2cdd8484daf3e63cffa0296c40098f7",
+    "url": "/MovieStar/index.html"
+  },
+  {
+    "revision": "f602f5a36d204767d640",
+    "url": "/MovieStar/static/css/main.06e0f23e.chunk.css"
+  },
+  {
+    "revision": "4b11a2ab3c60a7545958",
+    "url": "/MovieStar/static/js/2.7e0bab51.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MovieStar/static/js/2.7e0bab51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f602f5a36d204767d640",
+    "url": "/MovieStar/static/js/main.538be12a.chunk.js"
+  },
+  {
+    "revision": "1a46ead03714e0b4dd23",
+    "url": "/MovieStar/static/js/runtime-main.47cc8c6b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ecd0f4a253fe41d47509e16f864ce26.js"
+  "/MovieStar/precache-manifest.ac8253317f085c2101e00f4f520ba113.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MovieStar/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
